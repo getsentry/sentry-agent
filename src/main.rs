5cf1cd2dab5ce4002613abb96e8836a82628a805
Chad Whitacre <chadwhitacre@sentry.io>
2024-01-25 18:08:21 -0500

    DSN from environment

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,22 @@
-use std::time::Duration;
-use sentry::metrics::Metric;
 use sentry::metrics::FractionUnit;
+use sentry::metrics::Metric;
+use std::env;
+use std::time::Duration;
 use sysinfo::System;
 
 fn main() {
-    let mut sys = System::new_all();
-		let _guard = sentry::init(("https://55353bf1f574fa979e96733a2238eda6@o1.ingest.sentry.io/4506634220929024", sentry::ClientOptions {
+		let sentry_dsn = env::var("SENTRY_DSN").expect("SENTRY_DSN not set");
+		let _guard = sentry::init((sentry_dsn, sentry::ClientOptions {
 			release: sentry::release_name!(),
 			..Default::default()
 		}));
 
-    loop {
-        sys.refresh_memory();
-        let memory = sys.used_memory() as f64 / sys.total_memory() as f64;
-        println!("sending {}", memory);
-        Metric::distribution("memory", memory).with_unit(FractionUnit::Ratio).send();
-        std::thread::sleep(Duration::from_secs(15));
-    }
+		let mut sys = System::new_all();
+		loop {
+				sys.refresh_memory();
+				let memory = sys.used_memory() as f64 / sys.total_memory() as f64;
+				println!("sending {}", memory);
+				Metric::distribution("memory", memory).with_unit(FractionUnit::Ratio).send();
+				std::thread::sleep(Duration::from_secs(3));
+		}
 }
